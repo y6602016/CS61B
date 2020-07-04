005512b699217ce3817398204ff3a3153014f845
洪啟睿 <MIKE@hongqiruideMacBook-Pro.local>
2020-07-04 22:45:19 +0800

    update leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,21 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    private static boolean isLeapYear(int year){
+        if(year%4==0){
+            if(year%100==0){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
